e669ce9c7db08293c2ce613df23e1f050a3947a5
Melih Culfa <melihculfa@outlook.com>
2026-06-18 01:43:47 +0200

    Simulate failed test case

---
+++ test_logic.py
@@ -0,0 +1,15 @@
+import unittest
+from geometry import triangle_area, square_area
+
+
+class TestGeometry(unittest.TestCase):
+
+    def test_triangle(self):
+        self.assertEqual(triangle_area(10, 5), 25.0)
+
+    def test_square(self):
+        self.assertEqual(square_area(4), 999)
+
+
+if __name__ == "__main__":
+    unittest.main()
